42808b73a742603da83a0495369762379fda610c
rhodinemma <rhodinemma10@gmail.com>
2023-01-04 16:32:53 +0300

    ft(bootstrap-part1)

--- main.tf
+++ main.tf
@@ -11,7 +11,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164"
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "rhodin-tf-state"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
