078298ce8992ba58ea8f0b9eefd0171921a4133c
rhodinemma <rhodinemma10@gmail.com>
2023-01-04 16:43:30 +0300

    ft(bootstrap-part2)

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "rhodin-tf-state"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
